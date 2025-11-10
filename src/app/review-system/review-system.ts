a675e8cebfb4b7f8860688a86b14457c6b1d41db
Luke <lnalewaj@andrew.cmu.edu>
2025-11-10 14:05:41 -0500

    add review system component

---
+++ src/app/review-system/review-system.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-system',
+  imports: [],
+  templateUrl: './review-system.html',
+  styleUrl: './review-system.scss',
+})
+export class ReviewSystem {
+
+}
